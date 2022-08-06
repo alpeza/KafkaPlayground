40ada67b4a8a3da3453944c3738d684973f1a135
alpeza06 <alpeza>
2022-08-06 14:24:51 +0200

    up

--- tools/extractAllowedTopicsForAApp.sh
+++ tools/extractAllowedTopicsForAApp.sh
@@ -59,6 +59,5 @@ extractTopics
 #3.- Hacemos un join entre los topics actuales desplegados y las acls 
 #    de una determinada aplicacion
 getAllowedTopicsForApp "$paramApp"
-
 # Borramos ficheros temporales
 rm -f "$list_output_file" "$acls_output_matrix_file" "$topics_output_list_file" "$acls_output_list_file"
