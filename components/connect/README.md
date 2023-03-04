f71668924cfd3da0477faf4a56c6fd5968095c0b
alpeza <80060968+alpeza@users.noreply.github.com>
2023-03-04 15:04:24 +0100

    update

---
+++ components/connect/README.md
@@ -0,0 +1,8 @@
+# Confluent Connect
+
+## Conectores
+
+Se pueden chequear los conectores disponibles en [https://www.confluent.io/hub/](https://www.confluent.io/hub/)
+
+### Conectores disponibles en la instalación
+
--- components/terminal/scripts/plainproductor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Produce mensajes en texto plano hacia un determinado topic
-topic="test"
-
-
-for i in {1..100}
-do
-  echo "Este es el mensaje número $i"
-done | kafka-console-producer.sh --bootstrap-server "kafka:9092" --topic "$topic"
